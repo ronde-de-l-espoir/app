054674cd3f462e42c68408ec2455629b0d7b522d
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-04-26 19:54:02 +0200

    OK

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* import * as React from 'react'; */
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, BackHandler } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { Camera } from 'expo-camera';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const webViewRef = useRef(null);
+  const Spinner = () => (
+    <View style={styles.activityContainer}>
+      <ActivityIndicator size="large" color="#f29900" />
+    </View>
+  );
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+  useEffect(() => {
+    const backAction = () => {
+      webViewRef.current.goBack();
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        source={{ uri: 'http://app-www.ronde-de-l-espoir.fr' }}
+        ref={webViewRef}
+        style={styles.view}
+        originWhitelist={['*']}
+        allowsInlineMediaPlayback
+        javaScriptEnabled
+        scalesPageToFit
+        mediaPlaybackRequiresUserAction={false}
+        javaScriptEnabledAndroid
+        useWebkit
+        startInLoadingState={true}
+        renderLoading={Spinner}
+      />
     </View>
   );
 }
@@ -13,8 +66,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#888',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
